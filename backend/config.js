4f30195540c8db34eceb7ea3a0c5644c1e3dcfd1
Danahterek888 <52310106@students.liu.edu.lb>
2026-01-03 15:56:19 +0200

    env and config.js added

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const urlDB = `mysql://root:OxwEuBfOlDdeoFdMXJAESGvPQXtWckMt@mysql.railway.internal:3306/railway`
+
+const connection = mysql.createConnection(urlDB);
+
+module.exports = connection;
